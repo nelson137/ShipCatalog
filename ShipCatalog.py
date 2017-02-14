f12cfd3143fdd5a0fccf7ffe117245f27265bc7b
Nelson Earle <nelson.earle137@gmail.com>
2017-02-13 21:37:50 -0600

    removed sublime project files

--- ShipCatalog.py
+++ ShipCatalog.py
@@ -1,7 +1,13 @@
 from Ships import Ships
+from Tkinter import Tk
 
 def main():
     ships = Ships()
+
+    root = Tk()
+    root.title('Test')
+
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
